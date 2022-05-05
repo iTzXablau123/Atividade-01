71bd8b92561ff83d9e9a874b02fd53b300464bc2
Xablau <102757880+iTzXablau123@users.noreply.github.com>
2022-05-05 15:41:23 -0300

    Add files via upload

---
+++ Atividade 01 - JS.js
@@ -0,0 +1,30 @@
+function nome (q){
+    console.log(q)
+    return
+}
+
+nome ('Kevin')
+
+function soma (a,w){
+    let res = a+w;
+    console.log(res)
+    return
+}
+
+soma (2, 2)
+
+function mult (s,d){
+    let kell = s*d;
+    console.log(kell)
+    return
+}
+
+mult (2, 3)
+
+function meida (z, x, c, v){
+    let kr = (z+x+c+v)/4
+    console.log(kr)
+    return
+}
+
+media (9, 10, 10, 9)
